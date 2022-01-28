bc5b34ab486b7c3484868a631ee88cb4292cac2d
ArcOscar <oscarperezgutierrez77@gmail.com>
2022-01-28 12:59:49 -0600

    Changes on requirements.txt

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import pandas as pd
 import streamlit as st
-from PIL import Image
-import plotly.express as px
-import csv
 
-gss_data=pd.read_csv("./restaurantes.csv", delimiter=",")
+gss_data=pd.read_csv("restaurantes.csv")
 print("Data imported")
 
 st.dataframe(gss_data)
